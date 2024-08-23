16f8dd2bf12061c551e4d6c8022e709a61e17c25
jcat <jftcatarino@gmail.com>
2024-08-23 14:50:30 +0100

    idk

--- MessageParser.cpp
+++ MessageParser.cpp
@@ -1,24 +1,26 @@
 #include "MessageParser.hpp"
 
-bool	MessageParser::parseSource(const std::string::iterator& msg) {
+bool	MessageParser::parseSource(std::string::iterator& msg) {
 }
 
-bool	MessageParser::parsePlainCommand(const std::string::iterator& cursor) {
+bool	MessageParser::parsePlainCommand(std::string::iterator& cursor) {
 		if (cursor
 	return true;
 }
 
-bool	MessageParser::parseDigitCommand(const std::string::iterator& cursor) {
+bool	MessageParser::parseDigitCommand(std::string::iterator& cursor) {
 	return true;
 }
 
-bool	MessageParser::parseCommand(const std::string::iterator& msg)  {
+bool	MessageParser::parseCommand(std::string::iterator& msg)  {
 	if (!parsePlainCommand(msg))
 		return parseDigitCommand(msg);
 	return true;
 }
 
-bool	MessageParser::parseMessage(const std::string::iterator& cursor) {
+bool	MessageParser::parseMessage(const std::string& msg) {
+	std::string::iterator = msg.begin();
+
 	parseCommand(cursor);
 	parseParameters(cursor);
 	parseMessageEnd(cursor);
