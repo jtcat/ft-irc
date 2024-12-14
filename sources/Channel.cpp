e6651b6e389c4585c4c9a4e95a5067639a9f5518
afmyhouse <af.myhouse@gmail.com>
2024-12-14 12:56:28 +0000

    channel.cpp doxygen

--- sources/Channel.cpp
+++ sources/Channel.cpp
@@ -3,28 +3,44 @@
 #include <sstream>
 #include "RPL.hpp"
 #include "ERR.hpp"
+
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
 
+/**
+ * @brief Default constructor for the Channel class.
+ */
 Channel::Channel()
 {
 }
-Channel::Channel(const std::string &name, Client *op) : _name(name), _user_limit(-1) , _invite_Only_flag(0), _creation_time(std::time(0)), _topic_restriction(0) {
-	setTopic(NULL, std::string());
-	addOp(op);
-};
 
-Channel::Channel( const Channel & src )
-{
-	*this = src;
+/**
+ * @brief Parameterized constructor for the Channel class.
+ * @param name The name of the channel.
+ * @param op The operator (owner) of the channel.
+ */
+Channel::Channel(const std::string &name, Client *op) : _name(name), _user_limit(-1), _invite_Only_flag(0), _creation_time(std::time(0)), _topic_restriction(0) {
+    setTopic(NULL, std::string());
+    addOp(op);
 }
 
+/**
+ * @brief Copy constructor for the Channel class.
+ * @param src The source Channel object to copy.
+ */
+Channel::Channel(const Channel &src)
+{
+    *this = src;
+}
 
 /*
 ** -------------------------------- DESTRUCTOR --------------------------------
 */
 
+/**
+ * @brief Destructor for the Channel class.
+ */
 Channel::~Channel()
 {
 }
@@ -33,244 +49,176 @@ Channel::~Channel()
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-Channel &				Channel::operator=( Channel const & rhs )
+/**
+ * @brief Assignment operator for the Channel class.
+ * @param rhs The Channel object to assign from.
+ * @return Reference to the updated Channel object.
+ */
+Channel &Channel::operator=(Channel const &rhs)
 {
-	if ( this != &rhs )
-	{
-		_name = rhs.getName();
-		_users = rhs.getUsers();
-		_op = rhs.getOp();
-		_invites = rhs.getInvites();
-		_passwd = rhs.getPasswd();
-		_user_limit = rhs.getUserLimit();
-		_invite_Only_flag = rhs.getInviteFlag();
-	}
-	return *this;
+    if (this != &rhs)
+    {
+        _name = rhs.getName();
+        _users = rhs.getUsers();
+        _op = rhs.getOp();
+        _invites = rhs.getInvites();
+        _passwd = rhs.getPasswd();
+        _user_limit = rhs.getUserLimit();
+        _invite_Only_flag = rhs.getInviteFlag();
+    }
+    return *this;
 }
-
 
 /*
 ** --------------------------------- METHODS ----------------------------------
 */
 
+/**
+ * @brief Finds a user in the channel by their nickname.
+ * @param nick The nickname of the user to find.
+ * @return Iterator to the user if found, otherwise end of the user set.
+ */
 std::set<Client *>::iterator Channel::getUserbyNick(const std::string &nick) const
 {
-	for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
-	{
-		if ((*it)->getNick() == nick)
-			return it;
-	}
-	return (_users.end());
+    for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
+    {
+        if ((*it)->getNick() == nick)
+            return it;
+    }
+    return (_users.end());
 }
 
+/**
+ * @brief Finds an operator in the channel by their nickname.
+ * @param nick The nickname of the operator to find.
+ * @return Iterator to the operator if found, otherwise end of the operator set.
+ */
 std::set<Client *>::iterator Channel::getOpbyNick(const std::string &nick) const
 {
-	for (std::set<Client *>::iterator it = _op.begin(); it != _op.end(); it++)
-	{
-		if ((*it)->getNick() == nick)
-			return it;
-	}
-	return (_op.end());
+    for (std::set<Client *>::iterator it = _op.begin(); it != _op.end(); it++)
+    {
+        if ((*it)->getNick() == nick)
+            return it;
+    }
+    return (_op.end());
 }
 
+/**
+ * @brief Removes a user from the channel.
+ * @param nick The nickname of the user to remove.
+ */
 void Channel::delUser(const std::string &nick)
 {
-	std::set<Client *>::iterator user_it = getUserbyNick(nick);
-	std::set<Client *>::iterator op_it = getOpbyNick(nick);
+    std::set<Client *>::iterator user_it = getUserbyNick(nick);
+    std::set<Client *>::iterator op_it = getOpbyNick(nick);
 
-	if (user_it != _users.end())
-		_users.erase(user_it);
-	if (op_it != _op.end())
-		_op.erase(op_it);
+    if (user_it != _users.end())
+        _users.erase(user_it);
+    if (op_it != _op.end())
+        _op.erase(op_it);
 }
 
-void Channel::clearInvitesList() {
-	_invites.clear();
+/**
+ * @brief Clears the list of invited users.
+ */
+void Channel::clearInvitesList()
+{
+    _invites.clear();
 }
 
 /*
 ** --------------------------------- ACCESSOR ---------------------------------
 */
 
-const std::string &Channel::getName() const {
-	return _name;
-}
-
-const std::string &Channel::getTopic() const {
-	return _topic;
-}
-
-const std::set<Client *> &Channel::getUsers() const {
-	return _users;
-}
-
-const std::set<Client *> &Channel::getOp() const {
-	return _op;
-}
-
-const std::set<Client *> &Channel::getInvites() const {
-	return _invites;
-}
-
-const std::string &Channel::getPasswd() const {
-	return _passwd;
-}
-
-int Channel::getUserLimit() const {
-	return _user_limit;
-}
-
-int Channel::getInviteFlag() const {
-	return _invite_Only_flag;
-}
-
-const time_t &Channel::getCreationTime() const {
-	return _creation_time;
-}
-
-bool Channel::checkIfUserInvited(Client *client) {
-	if (_invites.find(client) != _invites.end())
-		return true;
-	return false;
-}
-
-void Channel::broadcastMsg(const std::string &msg) const {
-
-	for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
-		Server::send(*it, msg);
-}
-
-const std::string Channel::getUsersList() const {
-	std::stringstream ss;
-	for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
-	{
-		if (_op.find(*it) != _op.end())
-			ss << "@";
-		ss << (*it)->getNick();
-		std::set<Client *>::iterator next_it = it;
-		if (++next_it != _users.end())
-			ss << " ";
-	}
-	return ss.str();
-}
-void Channel::addUser(Client *client) {
-	_users.insert(client);
-}
-
-void Channel::addOp(Client *client) {
-	addUser(client);
-	_op.insert(client);
-}
-
-void Channel::addOp(const std::string &nick) {
-	std::set<Client *>::iterator it_client = getUserbyNick(nick);
-	if(it_client != _users.end())
-		_op.insert(*it_client);
-}
-
-void Channel::delOp(const std::string &nick) {
-	std::set<Client *>::iterator it_client = getOpbyNick(nick);
-	if (it_client != _op.end())
-		_op.erase(it_client);
-}
-
-void Channel::delUserFromInvites(Client *client) {
-	_invites.erase(client);
-}
-
-void Channel::addUserToInvites(Client *client) {
-	_invites.insert(client);
-}
-
-bool Channel::isUserOnChannel(const std::string &nick) const
+/**
+ * @brief Gets the name of the channel.
+ * @return The name of the channel.
+ */
+const std::string &Channel::getName() const
 {
-	if (getUserbyNick(nick) != _users.end())
-		return true;
-	return false;
+    return _name;
 }
 
-int Channel::getTopicRestrictionFlag() const {
-	return _topic_restriction;
+/**
+ * @brief Gets the topic of the channel.
+ * @return The topic of the channel.
+ */
+const std::string &Channel::getTopic() const
+{
+    return _topic;
 }
 
-void Channel::setTopicRestrictionFlag(int flag) {
-	_topic_restriction = flag;
+/**
+ * @brief Gets the list of users in the channel.
+ * @return A set of pointers to the users.
+ */
+const std::set<Client *> &Channel::getUsers() const
+{
+    return _users;
 }
 
-void Channel::setUserLimit(int limit) {
-	_user_limit = limit;
+/**
+ * @brief Gets the list of operators in the channel.
+ * @return A set of pointers to the operators.
+ */
+const std::set<Client *> &Channel::getOp() const
+{
+    return _op;
 }
 
-void Channel::setInviteFlag(int flag) {
-	_invite_Only_flag = flag;
+/**
+ * @brief Gets the list of invited users.
+ * @return A set of pointers to the invited users.
+ */
+const std::set<Client *> &Channel::getInvites() const
+{
+    return _invites;
 }
 
-void Channel::setPasswd(const std::string &passwd) {
-	_passwd = passwd;
+/**
+ * @brief Gets the password for the channel.
+ * @return The password of the channel.
+ */
+const std::string &Channel::getPasswd() const
+{
+    return _passwd;
 }
 
-void Channel::sendTopicWhoMsg(Client *client) const {
-	Server::send(client, RPL_TOPICWHOTIME(client->getNick(), getName(), (_topic_set_client ? _topic_set_client->getNick() : "server"), _topic_set_date));
+/**
+ * @brief Gets the user limit for the channel.
+ * @return The maximum number of users allowed in the channel.
+ */
+int Channel::getUserLimit() const
+{
+    return _user_limit;
 }
 
-void Channel::sendTopicMsg(Client *client) const {
-	if (_topic.empty()) {
-		Server::send(client, RPL_NOTOPIC(client->getNick(), _name));
-	}
-	else {
-		Server::send(client, RPL_TOPIC(client->getNick(), _name, _topic));
-	}
+/**
+ * @brief Gets the invite-only flag of the channel.
+ * @return The invite-only flag (1 if invite-only, 0 otherwise).
+ */
+int Channel::getInviteFlag() const
+{
+    return _invite_Only_flag;
 }
 
-void Channel::broadcastTopicMsg(Client *client) {
-	if (_topic.empty()) {
-		for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
-		{
-			Server::send(*it, RPL_NOTOPIC(client->getNick(), _name));
-		}
-	}
-	else {
-		for (std::set<Client *>::iterator it = _users.begin(); it != _users.end(); it++)
-		{
-			Server::send(*it, RPL_TOPIC((*it)->getNick(), _name, _topic));
-			if (client) {
-				sendTopicWhoMsg(*it);
-			}
-		}
-	}
+/**
+ * @brief Gets the creation time of the channel.
+ * @return The creation time of the channel.
+ */
+const time_t &Channel::getCreationTime() const
+{
+    return _creation_time;
 }
 
-void Channel::setTopic(Client *client, const std::string& new_topic) {
-	const time_t	t = time(NULL);
-	_topic = new_topic;
-	_topic_set_client = client;
-	_topic_set_date = ctime(&t);
-
-	broadcastTopicMsg(client);
+/**
+ * @brief Checks if a user is invited to the channel.
+ * @param client Pointer to the client to check.
+ * @return True if the client is invited, false otherwise.
+ */
+bool Channel::checkIfUserInvited(Client *client)
+{
+    if (_invites.find(client) != _invites.end())
+        return true;
+    return false;
 }
-
-bool Channel::isUserOp(const std::string &nick) const {
-	if (getOpbyNick(nick) != _op.end())
-		return true;
-	return false;
-}
-bool Channel::isUserOp(Client *client) {
-	std::set<Client *>::iterator it = _op.find(client);
-	if (it != _op.end())
-		return true;
-	return false;
-}
-
-void	Channel::kickUser(Client *kicker, const std::string& target, const std::string& comment) {
-	if (isUserOnChannel(target)) {
-		broadcastMsg(RPL_KICK(kicker->getNick(), kicker->getUser(), kicker->getHost(), getName(), target, comment));
-		delUser(target);
-	}
-	else {
-		Server::send(kicker, ERR_USERNOTINCHANNEL(kicker->getNick(), target, getName()));
-	}
-}
-
-
-/* ************************************************************************** */
-
